cdea4a4bcf36584d4b487d313a769896928c2d9b
Mr. Chispa <4927473+rcortes100@users.noreply.github.com>
2020-01-11 02:46:54 -0600

    Update questions.js

--- questions.js
+++ questions.js
@@ -16,4 +16,470 @@ questions = [
             "g": 0
         }
     },
+    {
+        "question": "Revolution is the best way of achieving a socialist society.",
+        "i18n": {
+            "de": "Revolution ist der beste Weg zur sozialistischen Revolution.",
+            "es": "La revolución es la mejor manera de alcanzar una sociedad socialista."
+        },
+        "effect": {
+            "a": 10,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        },
+    },
+    {
+        "question": "The negative consequences of a revolution generally outweigh the advantages.",
+        "i18n": {
+            "de": "Die negativen Folgen einer Revolution überwiegen im Allgemeinen die Vorteile.",
+            "es": "Las consecuencias negativas de una revolución generalmente rebasan sus ventajas."
+        },
+        "effect": {
+            "a": -10,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+    {
+        "question": "Liberal democracy is a viable way of achieving a socialist society.",
+        "i18n": {
+            "de": "Die liberale Demokratie ist ein realistischer Weg, um eine sozialistische Gesellschaft zu erreichen.",
+            "es": "La democracia liberal es un modo viable de alcanzar una sociedad socialista."
+        },
+        "effect": {
+            "a": -10,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+    {
+        "question": "A socialist revolution is made inevitable by the conditions of capitalism.",
+        "i18n": {
+            "de": "Eine sozialistische Revolution wird durch die Bedingungen des Kapitalismus unvermeidlich gemacht.",
+            "es": "Una revolución socialista es inevitable debido a las condiciones del capitalismo."
+        },
+        "effect": {
+            "a": 5,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+    {
+        "question": "Workplace democracy within capitalism is an acceptable alternative to a complete socialization of the economy.",
+        "i18n": {
+            "de": "Die Demokratie am Arbeitsplatz im Kapitalismus ist eine akzeptable Alternative zu einer vollständigen Sozialisierung der Wirtschaft.",
+            "es": "La democracia laboral dentro del capitalismo es una alternativa aceptable a la socialización completa de la economía."
+        },
+        "effect": {
+            "a": -10,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+    {
+        "question": "Revolutionary violence is acceptable as long as the final outcome is positive.",
+        "i18n": {
+            "de": "Revolutionäre Gewalt ist akzeptabel, solange das Endergebnis positiv ist.",
+            "es": "La violencia revolucionaria es aceptable siempre y cuando el resultado final sea positivo."
+        },
+        "effect": {
+            "a": 5,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+    {
+        "question": "Redistributing wealth away from the wealthy through taxes is a viable way of defeating inequality.",
+        "i18n": {
+            "de": "Die Umverteilung von Reichtum der Reichen durch Steuern ist ein realistischer Weg, um die Ungleichheit zu besiegen.",
+            "es": "La redistribución de la riqueza a través de impuestos a los más ricos es una manera viable de vencer la desigualdad."
+        },
+        "effect": {
+            "a": -5,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+    {
+        "question": "Modern social democracy is a betrayal of left-wing values.",
+        "i18n": {
+            "de": "Die moderne Sozialdemokratie ist ein Verrat an linken Werten.",
+            "es": "La democracia social moderna traiciona los valores de izquierda."
+        },
+        "effect": {
+            "a": 5,
+            "b": 0,
+            "c": 0,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+    {
+        "question": "Oppressed peoples have the right to engage in a violent uprising when all other options have been exhausted.",
+        "i18n": {
+            "de": "Unterdrückte Völker haben das Recht, sich an einem gewaltsamen Aufstand zu beteiligen, wenn alle anderen Möglichkeiten ausgeschöpft sind.",
+            "es": "La gente oprimida tiene el derecho a recurrir a un levantamiento violento cuando todas las otras opciones han sido agotadas."
+        },
+        "effect": {
+            "a": 5,
+            "b": 0,
+            "c": 0,
+            "d": 5,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+	{
+        "question": "The means of production, such as factories and farms, must be publicly owned.",
+        "i18n": {
+            "de": "Die Produktionsmittel wie Fabriken und Farmen müssen in öffentlichem Besitz sein.",
+            "es": "Los medios de producción, como las fábricas y granjas, deben ser públicos."
+        },
+        "effect": {
+            "a": 5,
+            "b": 5,
+            "c": 0,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+    //scientific vs utopian
+    {
+        "question": "Material conditions and needs are the dominant drive behind socioeconomic changes.",
+        "i18n": {
+            "de": "Die materiellen Grundlagen und Bedürfnisse sind der Hauptantrieb für sozioökonomische Veränderungen.",
+            "es": "Las condiciones y necesidades materiales son el incentivo dominante detrás de los cambios socioeconómicos. "
+        },
+        "effect": {
+            "a": 0,
+            "b": 10,
+            "c": 0,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+    {
+        "question": "Socialism can only be fully achieved in developed industrialized societies.",
+        "i18n": {
+            "de": "Der Sozialismus kann nur in entwickelten Industriegesellschaften voll verwirklicht werden.",
+            "es": "El socialismo sólo puede ser alcanzado plenamente en sociedades industrializadas desarrolladas."
+        },
+        "effect": {
+            "a": 0,
+            "b": 5,
+            "c": 0,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+	{
+        "question": "''From each according to his ability, to each according to his needs'' is a good principle.",
+        "i18n": {
+            "de": "''Jeder nach seinen Fähigkeiten, jedem nach seinen Bedürfnissen'' ist ein guter Grundsatz.",
+            "es": ""Que cada quién provea conforme a su capacidad, y cada quién reciba conforme a su necesidad" es un buen principio."
+        },
+        "effect": {
+            "a": 0,
+            "b": 5,
+            "c": 0,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+    {
+        "question": "It is possible to peacefully convince the ruling class to conform to a socialist society.",
+        "i18n": {
+            "de": "Es ist möglich, die herrschende Klasse friedlich davon zu überzeugen, sich einer sozialistischen Gesellschaft zu richten.",
+            "es": "Es posible convencer a la clase dominante de conformar una sociedad socialista de manera pacífica."
+        },
+        "effect": {
+            "a": -5,
+            "b": -10,
+            "c": 0,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+    {
+        "question": "Capitalism will induce its own demise through contradictions that result in crisis.",
+        "i18n": {
+            "de": "Der Kapitalismus wird seinen eigenen Niedergang durch seine zentralen Widersprüchen herbeiführen, die zur Krise führen.",
+            "es": "El capitalismo provocará su propia caída debido a contradicciones que resultan en una crisis."
+        },
+        "effect": {
+            "a": 0,
+            "b": 5,
+            "c": 0,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+    {
+        "question": "Class conflict is a force that has influenced much of human history.",
+        "i18n": {
+            "de": "Klassenkonflikte sind eine Kraft, die einen Großteil der menschlichen Geschichte beeinflusst haben.",
+            "es": "El conflicto de clases es una fuerza que ha influido en gran parte de la historia humana."
+        },
+        "effect": {
+            "a": 0,
+            "b": 10,
+            "c": 0,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+    {
+        "question": "The establishment of socialism is a gradual process, rather than an instant or rapid one.",
+        "i18n": {
+            "de": "Die Etablierung des Sozialismus ist eher ein schrittweiser als ein sofortiger oder schneller Prozess.",
+            "es": "El establecimiento del socialismo es un proceso gradual, más que un proceso rápido o instantáneo."
+        },
+        "effect": {
+            "a": 0,
+            "b": 5,
+            "c": 0,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+    {
+        "question": "Society is chiefly driven by individuals and ideas.",
+        "i18n": {
+            "de": "Die Gesellschaft wird hauptsächlich von Individuen und Ideen angetrieben.",
+            "es": "La sociedad es conducida principalmente por individuos e ideas."
+        },
+        "effect": {
+            "a": 0,
+            "b": -10,
+            "c": 0,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+    {
+        "question": "Total economic and social equality is realistically possible to achieve.",
+        "i18n": {
+            "de": "Eine vollständige wirtschaftliche und soziale Gleichstellung ist realistisch erreichbar.",
+            "es": "La igualdad económica y social total es realmente posible de alcanzar."
+        },
+        "effect": {
+            "a": 0,
+            "b": -10,
+            "c": 0,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+    //central vs decentral
+    {
+        "question": "An economy is generally designed better when it is organized bottom-up rather than top-down.",
+        "i18n": {
+            "de": "Eine Wirtschaft ist im Allgemeinen besser konzipiert, wenn sie von unten nach oben und nicht von oben nach unten organisiert ist.",
+            "es": "Una economía generalmente está mejor diseñada cuando se organiza de abajo hacia arriba, que de arriba hacia abajo."
+        },
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": -10,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+    {
+        "question": "Local planners, rather than national planners, are more efficient at running a planned economy.",
+        "i18n": {
+            "de": "Lokale Planer sind effizienter als nationale Planer, wenn es um die Führung einer Planwirtschaft geht.",
+            "es": "Los planificadores locales son más eficientes que los planificadores nacionales para administrar una economía planificada."
+        },
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": -5,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+    {
+        "question": "A centrally planned economy based around computers is a concept worth investigating.",
+        "i18n": {
+            "de": "Eine zentral auf Computern basierende Planwirtschaft ist ein Konzept, das wert ist, untersucht zu werden.",
+            "es": "Una economía planificada centralizada basada en computadoras es un concepto que vale la pena investigar."
+        },
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 5,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+    {
+        "question": "In underdeveloped societies a centrally planned economy is the best way to make rapid progress.",
+        "i18n": {
+            "de": "In unterentwickelten Gesellschaften ist eine zentral geplante Wirtschaft der beste Weg, um rasche Fortschritte zu erzielen.",
+            "es": "En las sociedades subdesarrolladas, una economía planificada centralizada es la mejor manera de hacer progreso rápido."
+        },
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 10,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+    {
+        "question": "The principles of workplace democracy and self-management are desirable.",
+        "i18n": {
+            "de": "Die Grundsätze der Demokratie am Arbeitsplatz und der Selbstverwaltung sind wünschenswert.",
+            "es": "Los principios de la democracia laboral y la autogestión son deseables."
+        },
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": -5,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+	{
+        "question": "The state can be best defined as a monopoly on violence and oppression.",
+        "i18n": {
+            "de": "Der Staat kann am besten als Gewalt- und Unterdrückungsmonopol definiert werden.",
+            "es": "La mejor definición de "estado" es un monopolio de la violencia y la opresión."
+        },
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": -5,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+    {
+        "question": "A highly centralized planned economy is not socialism but rather state capitalism.",
+        "i18n": {
+            "de": "Eine stark zentralisierte Planwirtschaft ist nicht Sozialismus, sondern Staatskapitalismus.",
+            "es": "Una economía planificada centralizada no es socialismo, sino un capitalismo de estado."
+        },
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": -5,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+    {
+        "question": "Bureaucracy and inefficiency are always inherent in centrally planned economies.",
+        "i18n": {
+            "de": "Bürokratie und Ineffizienz sind in zentral geplanten Volkswirtschaften immer vorhanden.",
+            "es": "La burocracia e ineficiencia son siempre inherentes a las economías planificadas centralizadas."
+        },
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": -10,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+	{
+        "question": "It is necessary to establish a dictatorship of the proletariat as a transitionary stage between capitalism and socialism.",
+        "i18n": {
+            "de": "Es ist notwendig, eine Diktatur des Proletariats als Übergangsphase zwischen Kapitalismus und Sozialismus zu etablieren.",
+            "es": "Es necesario establecer una dictadura del proletariado como etapa transitoria del capitalismo al socialismo."
+        },
+        "effect": {
+            "a": 0,
+            "b": 5,
+            "c": 5,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
+    {
+        "question": "The government should be capable of assigning individuals to workplaces.",
+        "i18n": {
+            "de": "Die Regierung sollte in der Lage sein, Individuen Arbeitsplätzen zuzuweisen.",
+            "es": "El gobierno debería tener la capacidad de asignar individuos a los puestos de trabajo."
+        },
+        "effect": {
+            "a": 0,
+            "b": 0,
+            "c": 10,
+            "d": 0,
+            "e": 0,
+            "f": 0,
+            "g": 0
+        }
+    },
 ];
